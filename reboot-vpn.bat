0ab7c69214f5ed978930d0a4d5e61a97163a2f5c
negiac <lv-007@163.com>
2021-05-12 15:26:55 +0800

    windows 下重启vpn，程序重启

---
+++ reboot-vpn.bat
@@ -0,0 +1,22 @@
+@echo off
+echo "VPN自动检查重启服务正在进行中......."
+echo "停止服务请按 Ctrl+C"
+
+:begin
+echo %date% %time% "ping......" >>c:\ping.txt
+ping 10.52.0.140 >>c:\ping.txt
+rem echo %errorlevel%
+if %ERRORLEVEL% == 1 goto reboot
+goto loop
+
+:reboot
+echo %date% %time% "VPN已重新启动" >>c:\errlog.log
+echo %date% %time% "VPN停用中...."
+taskkill /f /im SecUI.exe /t
+echo %date% %time% "VPN启动中...."
+"C:\Program Files (x86)\V5VPN\SecUI.exe"
+echo %date% %time% "VPN已重新启动...."
+
+:loop
+ping 127.0.0.1 -n 300 > nul
+goto begin
